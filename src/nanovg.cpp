9294f529104da02eca6ad9b72bd23b21f71d128c
Ryan Alexander <ryan@onecm.com>
2014-10-26 18:14:00 -0700

    Use ES3 renderer when available

--- src/nanovg.cpp
+++ src/nanovg.cpp
@@ -3,7 +3,9 @@
 
 #include "cinder/gl/gl.h"
 
-#if defined(CINDER_GL_ES_2)
+#if defined(CINDER_GL_ES_3)
+#define NANOVG_GLES3_IMPLEMENTATION
+#elif defined(CINDER_GL_ES_2)
 #define NANOVG_GLES2_IMPLEMENTATION
 #else
 #define NANOVG_GL3_IMPLEMENTATION
@@ -19,7 +21,9 @@ Context Context::create(bool antiAlias, bool stencilStrokes) {
   int flags = (antiAlias      ? NVG_ANTIALIAS       : 0) |
               (stencilStrokes ? NVG_STENCIL_STROKES : 0);
 
-#if defined(NANOVG_GLES2)
+#if defined(NANOVG_GLES3)
+  return { nvgCreateGLES3(flags), nvgDeleteGLES3 };
+#elif defined(NANOVG_GLES2)
   return { nvgCreateGLES2(flags), nvgDeleteGLES2 };
 #else
   return { nvgCreateGL3(flags), nvgDeleteGL3 };
