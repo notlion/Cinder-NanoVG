463ec07596e3d7bbbe5035928db4aab10c59d30e
Ryan Alexander <ryan@onecm.com>
2015-02-16 16:24:41 -0800

    Formatting

--- src/nanovg.cpp
+++ src/nanovg.cpp
@@ -1,7 +1,8 @@
 #include "nanovg.hpp"
 #include "SvgRenderer.hpp"
 
-namespace cinder { namespace nvg {
+namespace cinder {
+namespace nvg {
 
 Context::Context(NVGcontext *ptr, Deleter deleter) : mPtr{ ptr, deleter } {
 }
@@ -15,10 +16,10 @@ NVGpaint Context::convert(const svg::Paint &paint) {
   // There is probably also a better way to handle this..
   assert(paint.isLinearGradient());
 
-  const auto& p0 = paint.getCoords0();
-  const auto& p1 = paint.getCoords1();
-  const auto& c0 = paint.getColor(0);
-  const auto& c1 = paint.getColor(1);
+  const auto &p0 = paint.getCoords0();
+  const auto &p1 = paint.getCoords1();
+  const auto &c0 = paint.getColor(0);
+  const auto &c1 = paint.getColor(1);
 
   return linearGradient(p0, p1, c0, c1);
 }
@@ -35,8 +36,8 @@ void Context::fillPaint(const svg::Paint &paint) {
   fillColor(paint.getColor());
 }
 
-void Context::polyLine(const PolyLine2f& polyline) {
-  auto& pts = polyline.getPoints();
+void Context::polyLine(const PolyLine2f &polyline) {
+  auto &pts = polyline.getPoints();
   if (pts.size() >= 2) {
     moveTo(pts[0]);
     for (auto i = begin(pts) + 1; i != end(pts); ++i) {
@@ -60,18 +61,17 @@ void Context::path2dSegment(Path2d::SegmentType type, const vec2 *p1,
 }
 
 void Context::path2d(const cinder::Path2d &path) {
-  const vec2* p1 = nullptr, *p2 = nullptr, *p3 = &path.getPoint(0);
+  const vec2 *p1 = nullptr, *p2 = nullptr, *p3 = &path.getPoint(0);
 
   path2dSegment(Path2d::MOVETO, p1, p2, p3);
 
   size_t i = 1;
-  for (auto& seg : path.getSegments()) {
-    if(seg == Path2d::CLOSE) {
+  for (auto &seg : path.getSegments()) {
+    if (seg == Path2d::CLOSE) {
       p1 = p2;
       p2 = p3;
       p3 = &path.getPoint(0);
-    }
-    else {
+    } else {
       for (size_t ni = i + Path2d::sSegmentTypePointCounts[seg]; i < ni; ++i) {
         p1 = p2;
         p2 = p3;
@@ -84,7 +84,7 @@ void Context::path2d(const cinder::Path2d &path) {
 }
 
 void Context::shape2d(const Shape2d &shape) {
-  for (auto& contour : shape.getContours()) {
+  for (auto &contour : shape.getContours()) {
     path2d(contour);
   }
 }
@@ -111,9 +111,15 @@ void Context::endFrame() {
 
 // State Handling //
 
-void Context::save() { nvgSave(get()); }
-void Context::restore() { nvgRestore(get()); }
-void Context::reset() { nvgReset(get()); }
+void Context::save() {
+  nvgSave(get());
+}
+void Context::restore() {
+  nvgRestore(get());
+}
+void Context::reset() {
+  nvgReset(get());
+}
 
 // Render Styles //
 
@@ -121,7 +127,7 @@ void Context::strokeColor(const NVGcolor &color) {
   nvgStrokeColor(get(), color);
 }
 void Context::strokeColor(const ColorAf &color) {
-  strokeColor(reinterpret_cast<const NVGcolor&>(color));
+  strokeColor(reinterpret_cast<const NVGcolor &>(color));
 }
 void Context::strokePaint(const NVGpaint &paint) {
   nvgStrokePaint(get(), paint);
@@ -130,26 +136,34 @@ void Context::fillColor(const NVGcolor &color) {
   nvgFillColor(get(), color);
 }
 void Context::fillColor(const ColorAf &color) {
-  fillColor(reinterpret_cast<const NVGcolor&>(color));
+  fillColor(reinterpret_cast<const NVGcolor &>(color));
 }
 void Context::fillPaint(const NVGpaint &paint) {
   nvgFillPaint(get(), paint);
 }
 
-void Context::miterLimit(float limit) { nvgMiterLimit(get(), limit); }
-void Context::strokeWidth(float size) { nvgStrokeWidth(get(), size); }
-void Context::lineCap(int cap) { nvgLineCap(get(), cap); }
-void Context::lineJoin(int join) { nvgLineJoin(get(), join); }
+void Context::miterLimit(float limit) {
+  nvgMiterLimit(get(), limit);
+}
+void Context::strokeWidth(float size) {
+  nvgStrokeWidth(get(), size);
+}
+void Context::lineCap(int cap) {
+  nvgLineCap(get(), cap);
+}
+void Context::lineJoin(int join) {
+  nvgLineJoin(get(), join);
+}
 
 // Transform //
 
 void Context::resetTransform() {
   nvgResetTransform(get());
 }
-void Context::transform(const MatrixAffine2f& mtx) {
+void Context::transform(const MatrixAffine2f &mtx) {
   nvgTransform(get(), mtx[0], mtx[1], mtx[2], mtx[3], mtx[4], mtx[5]);
 }
-void Context::setTransform(const MatrixAffine2f& mtx) {
+void Context::setTransform(const MatrixAffine2f &mtx) {
   resetTransform();
   transform(mtx);
 }
@@ -159,11 +173,21 @@ void Context::translate(float x, float y) {
 void Context::translate(const vec2 &translation) {
   translate(translation.x, translation.y);
 }
-void Context::rotate(float angle) { nvgRotate(get(), angle); }
-void Context::skewX(float angle) { nvgSkewX(get(), angle); }
-void Context::skewY(float angle) { nvgSkewY(get(), angle); }
-void Context::scale(float x, float y) { nvgScale(get(), x, y); }
-void Context::scale(const vec2 &s) { scale(s.x, s.y); }
+void Context::rotate(float angle) {
+  nvgRotate(get(), angle);
+}
+void Context::skewX(float angle) {
+  nvgSkewX(get(), angle);
+}
+void Context::skewY(float angle) {
+  nvgSkewY(get(), angle);
+}
+void Context::scale(float x, float y) {
+  nvgScale(get(), x, y);
+}
+void Context::scale(const vec2 &s) {
+  scale(s.x, s.y);
+}
 
 MatrixAffine2f Context::currentTransform() {
   MatrixAffine2f xform;
@@ -176,8 +200,8 @@ MatrixAffine2f Context::currentTransform() {
 NVGpaint Context::linearGradient(const vec2 &start, const vec2 &end,
                                  const ColorAf &startColor, const ColorAf &endColor) {
   return nvgLinearGradient(get(), start.x, start.y, end.x, end.y,
-                           reinterpret_cast<const NVGcolor&>(startColor),
-                           reinterpret_cast<const NVGcolor&>(endColor));
+                           reinterpret_cast<const NVGcolor &>(startColor),
+                           reinterpret_cast<const NVGcolor &>(endColor));
 }
 NVGpaint Context::boxGradient(const Rectf &bounds, float r, float f,
                               const ColorAf &startColor, const ColorAf &endColor) {
@@ -205,11 +229,21 @@ void Context::resetScissor() {
 
 // Paths //
 
-void Context::beginPath() { nvgBeginPath(get()); }
-void Context::moveTo(float x, float y) { nvgMoveTo(get(), x, y); }
-void Context::moveTo(const vec2 &p) { moveTo(p.x, p.y); }
-void Context::lineTo(float x, float y) { nvgLineTo(get(), x, y); }
-void Context::lineTo(const vec2 &p) { lineTo(p.x, p.y); }
+void Context::beginPath() {
+  nvgBeginPath(get());
+}
+void Context::moveTo(float x, float y) {
+  nvgMoveTo(get(), x, y);
+}
+void Context::moveTo(const vec2 &p) {
+  moveTo(p.x, p.y);
+}
+void Context::lineTo(float x, float y) {
+  nvgLineTo(get(), x, y);
+}
+void Context::lineTo(const vec2 &p) {
+  lineTo(p.x, p.y);
+}
 void Context::quadTo(float cx, float cy, float x, float y) {
   nvgQuadTo(get(), cx, cy, x, y);
 }
@@ -228,8 +262,12 @@ void Context::arcTo(float x1, float y1, float x2, float y2, float radius) {
 void Context::arcTo(const vec2 &p1, const vec2 &p2, float radius) {
   arcTo(p1.x, p1.y, p2.x, p2.y, radius);
 }
-void Context::closePath() { nvgClosePath(get()); }
-void Context::pathWinding(int dir) { nvgPathWinding(get(), dir); }
+void Context::closePath() {
+  nvgClosePath(get());
+}
+void Context::pathWinding(int dir) {
+  nvgPathWinding(get(), dir);
+}
 void Context::arc(float cx, float cy, float r, float a0, float a1, int dir) {
   nvgArc(get(), cx, cy, r, a0, a1, dir);
 }
@@ -239,13 +277,13 @@ void Context::arc(const vec2 &center, float r, float a0, float a1, int dir) {
 void Context::rect(float x, float y, float w, float h) {
   nvgRect(get(), x, y, w, h);
 }
-void Context::rect(const Rectf& r) {
+void Context::rect(const Rectf &r) {
   rect(r.getX1(), r.getY1(), r.getWidth(), r.getHeight());
 }
 void Context::roundedRect(float x, float y, float w, float h, float r) {
   nvgRoundedRect(get(), x, y, w, h, r);
 }
-void Context::roundedRect(const Rectf& rect, float r) {
+void Context::roundedRect(const Rectf &rect, float r) {
   roundedRect(rect.getX1(), rect.getY1(), rect.getWidth(), rect.getHeight(), r);
 }
 void Context::ellipse(float cx, float cy, float rx, float ry) {
@@ -261,22 +299,34 @@ void Context::circle(const vec2 &center, float radius) {
   circle(center.x, center.y, radius);
 }
 
-void Context::fill() { nvgFill(get()); }
-void Context::stroke() { nvgStroke(get()); }
+void Context::fill() {
+  nvgFill(get());
+}
+void Context::stroke() {
+  nvgStroke(get());
+}
 
 // Text //
 
-int Context::createFont(const std::string& name, const std::string& filename) {
+int Context::createFont(const std::string &name, const std::string &filename) {
   return nvgCreateFont(get(), name.c_str(), filename.c_str());
 }
-int Context::findFont(const std::string& name) {
+int Context::findFont(const std::string &name) {
   return nvgFindFont(get(), name.c_str());
 }
 
-void Context::fontSize(float size) { nvgFontSize(get(), size); }
-void Context::fontBlur(float blur) { nvgFontBlur(get(), blur); }
-void Context::fontFaceId(int font) { nvgFontFaceId(get(), font); }
-void Context::fontFace(const std::string& font) { nvgFontFace(get(), font.c_str()); }
+void Context::fontSize(float size) {
+  nvgFontSize(get(), size);
+}
+void Context::fontBlur(float blur) {
+  nvgFontBlur(get(), blur);
+}
+void Context::fontFaceId(int font) {
+  nvgFontFaceId(get(), font);
+}
+void Context::fontFace(const std::string &font) {
+  nvgFontFace(get(), font.c_str());
+}
 
 void Context::textLetterSpacing(float spacing) {
   nvgTextLetterSpacing(get(), spacing);
@@ -288,29 +338,29 @@ void Context::textAlign(int align) {
   nvgTextAlign(get(), align);
 }
 
-float Context::text(float x, float y, const std::string& str) {
+float Context::text(float x, float y, const std::string &str) {
   return nvgText(get(), x, y, str.c_str(), NULL);
 }
-float Context::text(const vec2 &p, const std::string& str) {
+float Context::text(const vec2 &p, const std::string &str) {
   return text(p.x, p.y, str);
 }
 
-void Context::textBox(float x, float y, float breakRowWidth, const std::string& str) {
+void Context::textBox(float x, float y, float breakRowWidth, const std::string &str) {
   nvgTextBox(get(), x, y, breakRowWidth, str.c_str(), NULL);
 }
-void Context::textBox(const vec2 &p, float breakRowWidth, const std::string& str) {
+void Context::textBox(const vec2 &p, float breakRowWidth, const std::string &str) {
   textBox(p.x, p.y, breakRowWidth, str);
 }
-void Context::textBox(const Rectf &rect, const std::string& str) {
+void Context::textBox(const Rectf &rect, const std::string &str) {
   textBox(rect.getX1(), rect.getY1(), rect.getWidth(), str);
 }
 
-Rectf Context::textBounds(float x, float y, const std::string& str) {
+Rectf Context::textBounds(float x, float y, const std::string &str) {
   Rectf bounds;
   nvgTextBounds(get(), x, y, str.c_str(), NULL, &bounds.x1);
   return bounds;
 }
-Rectf Context::textBounds(const vec2 &p, const std::string& str) {
+Rectf Context::textBounds(const vec2 &p, const std::string &str) {
   return textBounds(p.x, p.y, str);
 }
 
@@ -325,5 +375,4 @@ Rectf Context::textBoxBounds(const vec2 &p, float breakRowWidth, const std::stri
 Rectf Context::textBoxBounds(const Rectf &rect, const std::string &str) {
   return textBoxBounds(rect.getX1(), rect.getY1(), rect.getWidth(), str);
 }
-
 }} // cinder::nvg
